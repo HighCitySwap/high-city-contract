8cc78d1796c58a4b4f2e4beb0814f6afd3df70be
chanbook <648715275@qq.com>
2022-06-02 09:16:11 +0800

    hcc name update hcc gift market fix hcc purchase fix

--- HCCGiftNft.sol
+++ HCCGiftNft.sol
@@ -1777,12 +1777,13 @@ contract HCCGiftNft is ERC721Enumerable, Ownable {
     event CreateNft(address indexed owner, uint256 indexed id, uint256 indexed code);
     constructor(
         address _hccAddress,
-        address _otherPaymentAddress
+        address _otherPaymentAddress,
+        uint256 _hccPirce,
+        uint256 _otherPaymentPirce
     ) ERC721("HCCGift NFT", "HCCG NFT"){
         require(_hccAddress != address(0), "HCC address cannot be 0");
         require(_otherPaymentAddress != address(0), "Other payment address cannot be 0");
         hccAddress = _hccAddress;
-        otherPaymentAddress = _otherPaymentAddress;
     }
 
     modifier nftIsOpen {
